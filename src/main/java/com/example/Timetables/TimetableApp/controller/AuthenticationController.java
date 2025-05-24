6021565a74eb88d5bb0a73f31dcf13c4ebf1431a
HudsonCollier <hudsondcollier@gmail.com>
2025-05-24 15:31:19 -0400

    Finished authentication

---
+++ src/main/java/com/example/Timetables/TimetableApp/controller/AuthenticationController.java
@@ -0,0 +1,59 @@
+package com.example.Timetables.TimetableApp.controller;
+
+import com.example.Timetables.TimetableApp.dto.LoginUserDto;
+import com.example.Timetables.TimetableApp.dto.RegisterUserDto;
+import com.example.Timetables.TimetableApp.dto.VerifyUserDto;
+import com.example.Timetables.TimetableApp.model.User;
+import com.example.Timetables.TimetableApp.responses.LoginResponse;
+import com.example.Timetables.TimetableApp.service.AuthenticationService;
+import com.example.Timetables.TimetableApp.service.JwtService;
+import org.hibernate.metamodel.internal.RuntimeMetamodelsImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+        LoginResponse loginResponse = new LoginResponse(jwtToken, jwtService.getExpirationTime());
+        return ResponseEntity.ok(loginResponse);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<?> verifyUser(@RequestBody VerifyUserDto verifyUserDto) {
+        try {
+            authenticationService.verifyUser(verifyUserDto);
+            return ResponseEntity.ok("Account verified successfully");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/resend")
+    public ResponseEntity<?> resendVerificationCode(@RequestParam String email) {
+        try {
+            authenticationService.resendVerificationCode(email);
+            return ResponseEntity.ok("Verification code sent");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/example/Timetables/TimetableApp/service/UserService.java
@@ -0,0 +1,25 @@
+package com.example.Timetables.TimetableApp.service;
+
+import com.example.Timetables.TimetableApp.model.User;
+import com.example.Timetables.TimetableApp.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository, EmailService emailService) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+
+}
